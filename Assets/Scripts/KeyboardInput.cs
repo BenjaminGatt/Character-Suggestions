ad91d5c6f174f443b3845441fe647453474a6e0b
Benjamin Gatt <benjamin.gatt.f23393@mcast.edu.mt>
2020-11-29 14:37:29 +0100

    Commit 2

---
+++ Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour
+{
+    private float startTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            Debug.Log("pressed e");
+            startTime = Time.time;
+        }
+        if (Input.GetKey("e") && (Time.time - startTime > 2f))
+        {
+            Debug.Log("e ê é è ë ē ĕ ě ẽ ė ẹ ę ẻ");
+        }
+    }
+}
